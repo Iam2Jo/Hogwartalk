3f2e6c4bd39c09b1bc4ff6e83d94bb1d0a5791c4
어승준 <eoseungjun7@naver.com>
2023-11-07 16:07:14 +0900

    Env: app router

---
+++ app/home/myhouse/page.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const myhouse: NextPage = () => {
+  return (
+    <h1>
+      myhouse Page ㅋㅋㅋeerer
+    </h1>
+  );
+};
+
+export default myhouse;
---
+++ app/home/page.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const home: NextPage = () => {
+  return (
+    <h1>
+      home Page ㅋㅋㅋfff
+    </h1>
+  );
+};
+
+export default home;
