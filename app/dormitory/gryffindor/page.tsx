aa6507e2ee4e04554a41c7ee8d1564d2ef29b8f2
222SeungHyun <93538221+seungsimdang@users.noreply.github.com>
2023-11-09 00:51:50 +0900

    Feat: 기숙사별 채팅방 생성 및 채팅방 참여 기능 구현

--- app/dormitory/gryffindor/page.tsx
+++ app/dormitory/gryffindor/page.tsx
@@ -1,6 +1,59 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect } from 'react';
+import { io } from 'socket.io-client';
+import { gryffindorChatIdState } from '@recoil/dormChatId';
+import { useRecoilValue } from 'recoil';
+import axios from 'axios';
+
+interface RequestBody {
+  chatId: string;
+}
 
 const Gryffindor = () => {
+  const gryffindorChatId = useRecoilValue(gryffindorChatIdState);
+
+  const SERVER_KEY = '660d616b';
+  const ACCESS_TOKEN_HARRY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MGQ2MTZiOmhhcnJ5cG90dGVyIiwiaWF0IjoxNjk5MzQ1NDkzLCJleHAiOjE2OTk5NTAyOTN9.b5s4_9f-pVBj9ki17SXc6VvoiApMJZCJXfk5G2wskyo';
+  const ACCESS_TOKEN_HERMIONE =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MGQ2MTZiOmhlcm1pb25lIiwiaWF0IjoxNjk5NDIzOTI4LCJleHAiOjE3MDAwMjg3Mjh9.9FA24mkoipWSd4KlpxTX0L8mKmJj7LAVd_XEcW1Xt7w';
+  const PARTICIPATE_CHAT_URL = 'https://fastcampus-chat.net/chat/participate';
+
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${ACCESS_TOKEN_HERMIONE}`,
+    serverId: SERVER_KEY,
+  };
+
+  const requestData: RequestBody = {
+    chatId: gryffindorChatId,
+  };
+
+  useEffect(() => {
+    axios
+      .patch(PARTICIPATE_CHAT_URL, requestData, { headers })
+      .then((response) => {
+        console.log('Gryffindor 채팅방 참여 완료', response.data);
+
+        const socket = io(
+          `https://fastcampus-chat.net/chat?chatId=${gryffindorChatId}`,
+          {
+            extraHeaders: headers,
+          },
+        );
+
+        // socket.emit('message-to-server', 'test');
+
+        // socket.on('message-to-client', (messageObject) => {
+        //   console.log(messageObject);
+        // });
+      })
+      .catch((error) => {
+        console.error('Error sending the request:', error);
+      });
+  }, []);
+
   return <div>그리핀도르</div>;
 };
 
--- app/selectDormitory/selectDormitory.tsx
+++ app/selectDormitory/selectDormitory.tsx
@@ -1,66 +1,143 @@
 'use client';
 
-import type { NextPage } from 'next'
-import * as styled from './selectDormitory.styles'
-import cSocket from '@hooks/createChatting';
-import rSocket from '@hooks/readChatting';
-import { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import * as styled from './selectDormitory.styles';
 import Link from 'next/link';
+import axios from 'axios';
+import {
+  gryffindorChatIdState,
+  hufflepuffChatIdState,
+  ravenclawChatIdState,
+  slytherinChatIdState,
+} from '@recoil/dormChatId';
+import { useRecoilState } from 'recoil';
 
-const SelectDormitory: NextPage = () => {
+interface RequestBody {
+  name: string;
+  users: string[];
+  isPrivate?: boolean;
+}
 
-//   useEffect(()=>{
-//     cSocket.connect();
+const SelectDormitory = () => {
+  const [gryffindorChatId, setGryffindorChatId] = useRecoilState(
+    gryffindorChatIdState,
+  );
+  const [hufflepuffChatId, setHufflepuffChatId] = useRecoilState(
+    hufflepuffChatIdState,
+  );
+  const [ravenclawChatId, setRavenclawChatId] =
+    useRecoilState(ravenclawChatIdState);
+  const [slytherinChatId, setSlytherinChatId] =
+    useRecoilState(slytherinChatIdState);
 
-//     return () => {
-//       cSocket.disconnect();
-//     };
-//   },[])
+  const SERVER_KEY = '660d616b';
+  const ACCESS_TOKEN =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MGQ2MTZiOmhhcnJ5cG90dGVyIiwiaWF0IjoxNjk5MzQ1NDkzLCJleHAiOjE2OTk5NTAyOTN9.b5s4_9f-pVBj9ki17SXc6VvoiApMJZCJXfk5G2wskyo';
+  const CREATE_CHAT_URL = 'https://fastcampus-chat.net/chat';
 
-  useEffect(()=>{
-    console.log('a')
-    rSocket.connect();
-    console.log('b')
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${ACCESS_TOKEN}`,
+    serverId: SERVER_KEY,
+  };
 
-    return () => {
-        rSocket.disconnect();
-    };
-  },[])
+  const gryffindorRequestData: RequestBody = {
+    name: 'gryffindor',
+    users: ['ron'],
+  };
+
+  const hufflepuffRequestData: RequestBody = {
+    name: 'hufflepuff',
+    users: ['ron'],
+  };
+
+  const ravenclawRequestData: RequestBody = {
+    name: 'ravenclaw',
+    users: ['ron'],
+  };
+
+  const slytherinRequestData: RequestBody = {
+    name: 'slytherin',
+    users: ['ron'],
+  };
+
+  useEffect(() => {
+    axios
+      .post(CREATE_CHAT_URL, gryffindorRequestData, { headers })
+      .then((response) => {
+        console.log('Gryffindor 채팅방 생성 완료', response.data);
+        setGryffindorChatId(response.data.id);
+      })
+      .catch((error) => {
+        console.error('Error sending the request:', error);
+      });
+
+    axios
+      .post(CREATE_CHAT_URL, hufflepuffRequestData, { headers })
+      .then((response) => {
+        console.log('Hufflepuff 채팅방 생성 완료', response.data);
+        setHufflepuffChatId(response.data.id);
+      })
+      .catch((error) => {
+        console.error('Error sending the request:', error);
+      });
+
+    axios
+      .post(CREATE_CHAT_URL, ravenclawRequestData, { headers })
+      .then((response) => {
+        console.log('Ravenclaw 채팅방 생성 완료', response.data);
+        setRavenclawChatId(response.data.id);
+      })
+      .catch((error) => {
+        console.error('Error sending the request:', error);
+      });
+
+    axios
+      .post(CREATE_CHAT_URL, slytherinRequestData, { headers })
+      .then((response) => {
+        console.log('Slytherin 채팅방 생성 완료', response.data);
+        setSlytherinChatId(response.data.id);
+      })
+      .catch((error) => {
+        console.error('Error sending the request:', error);
+      });
+  }, []);
 
   return (
     <styled.Wrapper>
-    <styled.LeftSection>
-      <Link
-        href="/dormitory/gryffindor"
-        style={{ width: '100%', height: '100%' }}
-      >
-        <styled.GryffindorSVG />
-      </Link>
-      <Link
-        href="/dormitory/ravenclaw"
-        style={{ width: '100%', height: '100%' }}
-      >
-        <styled.RavenclawSVG />
-      </Link>
-    </styled.LeftSection>
-    <styled.CenterSection>
-      <styled.ClubSVG />
-    </styled.CenterSection>
-    <styled.RightSection>
-      <Link
-        href="/dormitory/hufflepuff"
-        style={{ width: '100%', height: '100%' }}
-      >
-        <styled.HufflepuffSVG />
-      </Link>
-      <Link
-        href="/dormitory/slytherin"
-        style={{ width: '100%', height: '100%' }}
-      >
-        <styled.SlytherinSVG />
-      </Link>
-    </styled.RightSection>
-  </styled.Wrapper>
-  )
-  }
+      <styled.LeftSection>
+        <Link
+          href="/dormitory/gryffindor"
+          style={{ width: '100%', height: '100%' }}
+        >
+          <styled.GryffindorSVG />
+        </Link>
+        <Link
+          href="/dormitory/ravenclaw"
+          style={{ width: '100%', height: '100%' }}
+        >
+          <styled.RavenclawSVG />
+        </Link>
+      </styled.LeftSection>
+      <styled.CenterSection>
+        <styled.ClubSVG />
+      </styled.CenterSection>
+      <styled.RightSection>
+        <Link
+          href="/dormitory/hufflepuff"
+          style={{ width: '100%', height: '100%' }}
+        >
+          <styled.HufflepuffSVG />
+        </Link>
+        <Link
+          href="/dormitory/slytherin"
+          style={{ width: '100%', height: '100%' }}
+        >
+          <styled.SlytherinSVG />
+        </Link>
+      </styled.RightSection>
+    </styled.Wrapper>
+  );
+};
+
 export default SelectDormitory;
