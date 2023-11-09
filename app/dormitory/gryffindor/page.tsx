46e99c391a3ff3ac7ec7024d4ee8e5159b296e89
어승준 <eoseungjun7@naver.com>
2023-11-09 18:37:47 +0900

    Feat: 채팅방 생성 및 기숙사 테스트

--- app/dormitory/gryffindor/page.tsx
+++ app/dormitory/gryffindor/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React, { use, useEffect, useState } from 'react';
+import { createChatting } from '@hooks/createChatting';
+import { readChatting } from '@hooks/readChatting';
 import { io } from 'socket.io-client';
 import { gryffindorChatIdState } from '@recoil/dormChatId';
 import { useRecoilValue } from 'recoil';
@@ -9,28 +11,20 @@ import axios from 'axios';
 interface RequestBody {
   chatId: string;
 }
-'use client';
 
-import React, { useEffect, useState } from 'react';
-import { createChatting } from '@hooks/createChatting';
-import { readChatting } from '@hooks/readChatting';
-
-type ResponseValue = any
+type ResponseValue = any;
 // type ResponseValue = Chat[]
 
-interface Chat {
-  id: string;
-  name: string;
-  users: User[]; // 속한 유저 정보
-  isPrivate: boolean;
-  latestMessage: Message | null;
-  updatedAt: Date;
-}
+type RequestData = string;
 
 interface User {
   id: string;
   name: string;
   picture: string;
+}
+
+interface ResponseData {
+  messages: Message[];
 }
 
 interface Message {
@@ -41,7 +35,96 @@ interface Message {
 }
 
 const Gryffindor = () => {
-  return <div>그리핀도르</div>;
+  const gryffindorChatId = useRecoilValue(gryffindorChatIdState);
+  const DORMITORY_NAME = 'gryffindor';
+  const data: ResponseValue | null = readChatting();
+  const [chatData, setChatData] = useState<ResponseValue | null>();
+  const [text, setText] = useState<RequestData>('');
+  const [previousMessages, setPreviousMessages] = useState<Message[]>([]);
+
+  const SERVER_KEY = '660d616b';
+  const ACCESS_TOKEN_HARRY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MGQ2MTZiOmhhcnJ5cG90dGVyIiwiaWF0IjoxNjk5MzQ1NDkzLCJleHAiOjE2OTk5NTAyOTN9.b5s4_9f-pVBj9ki17SXc6VvoiApMJZCJXfk5G2wskyo';
+  const ACCESS_TOKEN_HERMIONE =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MGQ2MTZiOmhlcm1pb25lIiwiaWF0IjoxNjk5NDIzOTI4LCJleHAiOjE3MDAwMjg3Mjh9.9FA24mkoipWSd4KlpxTX0L8mKmJj7LAVd_XEcW1Xt7w';
+  const PARTICIPATE_CHAT_URL = 'https://fastcampus-chat.net/chat/participate';
+
+  const headers = {
+    Authorization: `Bearer ${ACCESS_TOKEN_HERMIONE}`,
+    serverId: SERVER_KEY,
+  };
+
+  const requestData: RequestBody = {
+    chatId: gryffindorChatId,
+  };
+
+  const serverSocket = io(`https://fastcampus-chat.net/server`, {
+    extraHeaders: headers,
+  });
+
+  const chatSocket = io(
+    `https://fastcampus-chat.net/chat?chatId=${gryffindorChatId}`,
+    {
+      extraHeaders: headers,
+    },
+  );
+
+  const sendMessage = () => {
+    try {
+      if (text.trim() !== '') {
+        chatSocket.emit('message-to-server', text);
+        setText('');
+        console.log('C->S 메시지 전송 성공!');
+      }
+    } catch (error) {
+      console.error('C->S 메시지 전송 실패!', error);
+    }
+  };
+
+  useEffect(() => {
+    try {
+      chatSocket.emit('fetch-messages');
+      console.log('C->S 이전 대화목록 가져오기 성공!');
+    } catch (error) {
+      console.error('C->S 이전 대화목록 가져오기 실패!', error);
+    }
+  }, []);
+
+  useEffect(() => {
+    try {
+      chatSocket.on('messages-to-client', (messageObject: any) => {
+        console.log(messageObject);
+        setPreviousMessages(messageObject.messages);
+        console.log('S->C 이전 대화목록 가져오기 성공!');
+      });
+    } catch (error) {
+      console.error('S->C 이전 대화목록 가져오기 실패!', error);
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log('gryffindorChatId', gryffindorChatId);
+  }, [gryffindorChatId]);
+
+  return (
+    <div>
+      <div>그리핀도르</div>
+      <div>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button onClick={sendMessage}>Send</button>
+      </div>
+      {previousMessages.map((message) => (
+        <div key={message.id}>
+          <span>{message.userId}: </span>
+          <span>{message.text}</span>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Gryffindor;
