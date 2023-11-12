3d403202d36804917030a93083d3c8839fc09587
어승준 <eoseungjun7@naver.com>
2023-11-12 19:06:39 +0900

    Feat: 참여중인 대화방 구현, 이동 + latestMessage 폴링으로 받아오기

--- app/selectDormitory/gryffindor/page.tsx
+++ app/selectDormitory/gryffindor/page.tsx
@@ -1,11 +1,20 @@
 import type { NextPage } from 'next';
 import Gryffindor from './gryffindor';
+import MyChatting from './myChatting';
 
 const DormGryffindor: NextPage = () => {
   return (
-    <>
+    <div
+      style={{
+        display: 'flex',
+        gap: '1rem',
+        padding: '1rem',
+        overflowY: 'hidden',
+      }}
+    >
+      <MyChatting />
       <Gryffindor />
-    </>
+    </div>
   );
 };
 
