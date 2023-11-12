a8b604a33ee8bfd813cb6c0b7d429ae418d06ec1
moonyah <dltk456@naver.com>
2023-11-12 16:57:23 +0900

    Feat: 접속 유무 불러오기 추가

--- components/FriendSearchToggle/FriendSearchToggle.tsx
+++ components/FriendSearchToggle/FriendSearchToggle.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 import * as Styles from './FriendSearchToggle.styles';
 
 interface FriendSearchToggleProps {
@@ -10,6 +11,7 @@ interface User {
   id: string;
   name: string;
   picture: string;
+  isOnline?: boolean;
 }
 
 const FriendSearchToggle: React.FC<FriendSearchToggleProps> = ({
@@ -19,43 +21,76 @@ const FriendSearchToggle: React.FC<FriendSearchToggleProps> = ({
   const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const accessTokenCookie = document.cookie
-          .split('; ')
-          .find((row) => row.startsWith('acessToken='));
-
-        if (!accessTokenCookie) {
-          // 쿠키에 액세스 토큰이 없는 경우
-          return;
-        }
-        const accessToken = accessTokenCookie.split('=')[1];
-
-        const serverId = 'nREmPe9B';
-
-        const response = await fetch('https://fastcampus-chat.net/users', {
-          method: 'GET',
-          headers: {
-            'content-type': 'application/json',
-            serverId: serverId,
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
-
-        if (response.ok) {
-          const data: User[] = await response.json();
-          setUsers(data);
-          alert('Success');
-        } else {
-          console.error('Failed to fetch users');
-        }
-      } catch (error) {
-        console.error('Error fetching users', error);
-      }
-    };
-
     fetchUsers();
-  }, []);
+    const accessTokenCookie = document.cookie
+      .split('; ')
+      .find((row) => row.startsWith('accessToken='));
+
+    if (!accessTokenCookie) {
+      console.error('Access token not found in cookies');
+      return;
+    }
+
+    const accessToken = accessTokenCookie.split('=')[1];
+
+    const serverId = '660d616b';
+
+    const socket = io('https://fastcampus-chat.net', {
+      extraHeaders: {
+        'content-type': 'application/json',
+        Authorization: `Bearer ${accessToken}`,
+        serverId: serverId,
+      },
+    });
+
+    socket.on('users-to-client', (data: { user: string[] }) => {
+      const onlineUsers = data.user.map((userId) => ({
+        id: userId,
+        isOnline: true,
+        ...users.find((u) => u.id === userId),
+      }));
+      setUsers(onlineUsers);
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, [users]);
+
+  const fetchUsers = async () => {
+    try {
+      const accessTokenCookie = document.cookie
+        .split('; ')
+        .find((row) => row.startsWith('accessToken='));
+
+      if (!accessTokenCookie) {
+        console.error('Access token not found in cookies');
+        return;
+      }
+
+      const accessToken = accessTokenCookie.split('=')[1];
+
+      const serverId = '660d616b';
+
+      const response = await fetch('https://fastcampus-chat.net/users', {
+        method: 'GET',
+        headers: {
+          'content-type': 'application/json',
+          serverId: serverId,
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      if (response.ok) {
+        const data: User[] = await response.json();
+        setUsers(data);
+      } else {
+        console.error('Failed to fetch users');
+      }
+    } catch (error) {
+      console.error('Error fetching users', error);
+    }
+  };
 
   return (
     <Styles.Sidebar isVisible={isVisible}>
@@ -76,7 +111,8 @@ const FriendSearchToggle: React.FC<FriendSearchToggleProps> = ({
             />
             <Styles.UserInfo>
               <Styles.Username>
-                {user.name} <Styles.Emoji>🟢</Styles.Emoji>
+                {user.name}{' '}
+                <Styles.Emoji>{user.isOnline ? '🟢' : '🔴'}</Styles.Emoji>
               </Styles.Username>
             </Styles.UserInfo>
           </Styles.UserItem>
