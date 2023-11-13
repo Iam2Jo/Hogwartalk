23f9add8cf72c71bbd05372f1fe43ed63c5372b8
어승준 <eoseungjun7@naver.com>
2023-11-13 11:02:36 +0900

    Fix: 페이지에서 import x

--- app/selectDormitory/hufflepuff/page.tsx
+++ app/selectDormitory/hufflepuff/page.tsx
@@ -1,21 +1,20 @@
 import type { NextPage } from 'next';
 import Hufflepuff from './hufflepuff';
 import { MyChatting } from '@components/MyChatting';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  display: flex;
-  gap: 1rem;
-  padding: 1rem;
-  background-color: #1f1f1f;
-`;
 
 const DormHufflepuff: NextPage = () => {
   return (
-    <Container>
+    <div
+      style={{
+        display: 'flex',
+        gap: '1rem',
+        padding: '1rem',
+        backgroundColor: '#1f1f1f',
+      }}
+    >
       <MyChatting />
       <Hufflepuff />
-    </Container>
+    </div>
   );
 };
 
--- app/selectDormitory/slytherin/page.tsx
+++ app/selectDormitory/slytherin/page.tsx
@@ -1,21 +1,20 @@
 import type { NextPage } from 'next';
 import Slytherin from './slytherin';
 import { MyChatting } from '@components/MyChatting';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  display: flex;
-  gap: 1rem;
-  padding: 1rem;
-  background-color: #1f1f1f;
-`;
 
 const DormSlytherin: NextPage = () => {
   return (
-    <Container>
+    <div
+      style={{
+        display: 'flex',
+        gap: '1rem',
+        padding: '1rem',
+        backgroundColor: '#1f1f1f',
+      }}
+    >
       <MyChatting />
       <Slytherin />
-    </Container>
+    </div>
   );
 };
 
