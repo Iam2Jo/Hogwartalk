cdaf57524921f9ff69f49ce812a3b64241559d1f
kyu <qorudrb14@naver.com>
2023-11-15 14:10:06 +0900

    Feat:액세스토큰 만료시 갱신기능 구현

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Fragment, useState } from 'react';
 import cookies from 'react-cookies';
 import { LoginContainer, LoginFormStyle } from './loginStyle';
 import { useRouter } from 'next/navigation';
-import { getToken, getUserdata, loginUser } from '../utils/service';
+import { reissueAccessToken, getToken, getUserdata, loginUser } from '../utils/service';
 type LoginData = {
   id: string;
   password: string;
@@ -22,16 +22,17 @@ const main: NextPage = () => {
   const handleButtonClick = async () => {
     try {
       const token = await loginUser(loginData);
-    cookies.save('accessToken', token?.accessToken, { maxAge: 3600 });
+    cookies.save('accessToken', token?.accessToken, { maxAge: 3600 * 24 * 7});
     cookies.save('refreshToken', token?.refreshToken);
-    const rest = await getUserdata();
-    console.log(rest);
+    const userData = await getUserdata();
+    router.push('/selectDormitory');
     }
     catch (error) {
-      alert('아이디와 비밀번호가 올바른지 확인해주세요')
-      throw new Error(error)
+      alert('아이디와 비밀번호가 올바른지 확인해주세요');
+      throw new Error(error);
     }
   };
+  getUserdata();
 
   return (
     <LoginContainer>
